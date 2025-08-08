230662b0cd7b06068e906433e3aa6385171a12f2
amores <amores.dev@outlook.com>
2025-08-08 09:44:28 -0600

    agregar plantillas

---
+++ resources/views/tablero.blade.php
@@ -0,0 +1,12 @@
+@extends ('layouts.app')
+
+@section('content')
+
+    <!-- Aquí va el contenido específico del tablero -->
+    <div class="card">
+        <div class="card-body">
+            Bienvenido a tu agenda personal. Aquí puedes ver tus eventos y tareas pendientes.
+        </div>
+    </div>
+
+@endsection
