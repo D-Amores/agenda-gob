ab4256bd09f8095b9d1b1b835c51fa3d3990976d
amores <amores.dev@outlook.com>
2025-08-19 22:35:22 -0600

    feat: actualiza evento en la bd

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -41,7 +41,7 @@ class EventoController extends Controller
         ->exists();
 
         if ($exists) {
-            Alert::warning('Advertencia', 'Ya existe una audiencia con ese nombre en esa fecha y hora.')->autoClose(5000)->timerProgressBar();
+            Alert::warning('Advertencia', 'Ya existe un Evento con ese nombre en esa fecha y hora.')->autoClose(5000)->timerProgressBar();
             return back()->withInput();
         }
 
@@ -73,5 +73,62 @@ class EventoController extends Controller
     public function registrar(){
         $estatusLista = Estatus::all();
         return view('evento.registro', compact('estatusLista'));
+    }
+
+    public function editar(Evento $evento){
+        $estatusLista = Estatus::all();
+        $evento->fecha_evento = \Carbon\Carbon::parse($evento->fecha_evento)->format('Y-m-d');
+        return view('evento.editar', compact('evento', 'estatusLista'));
+    }
+
+    public function actualizar(Request $request, Evento $evento){
+        $request->merge([
+            'hora_evento' => substr($request->hora_evento, 0, 5), // Recorta a H:i
+            'hora_fin_evento' => substr($request->hora_fin_evento, 0, 5),
+        ]);
+
+        $validated = $request->validate([
+            'formValidationName' => 'required|string|min:10|max:255',
+            'asistenciaGobernador' => ['required', 'in:1,0'],
+            'formValidationLugar' => 'required|string|min:10|max:255',
+            'formValidationFecha' => 'required|date',
+            'vestimenta' => ['required', 'exists:vestimentas,id'],
+            'hora_evento' => 'required|date_format:H:i',
+            'hora_fin_evento' => 'required|date_format:H:i|after:hora_evento',
+            'estatus_id' => 'required|exists:estatus,id',
+            'descripcion' => 'nullable|string|max:500',
+        ]);
+
+        $exists = Evento::where('nombre', $request->formValidationName)
+        ->whereDate('fecha_evento', $request->formValidationFecha)
+        ->where('hora_evento', $request->hora_evento)
+        ->where('id', '!=', $evento->id)
+        ->exists();
+
+        if ($exists) {
+            Alert::warning('Advertencia', 'Ya existe un Evento con ese nombre en esa fecha y hora.')->autoClose(5000)->timerProgressBar();
+            return back()->withInput();
+        }
+
+        try {
+            $evento->update([
+                'nombre' => $validated['formValidationName'],
+                'asistencia_de_gobernador' => $validated['asistenciaGobernador'],
+                'lugar' => $validated['formValidationLugar'],
+                'fecha_evento' => $validated['formValidationFecha'],
+                'hora_evento' => $validated['hora_evento'],
+                'hora_fin_evento' => $validated['hora_fin_evento'],
+                'vestimenta_id' => $validated['vestimenta'], // mapeo del input al campo
+                'estatus_id' => $validated['estatus_id'],
+                'descripcion' => $validated['descripcion'] ?? null,
+            ]);
+
+            Alert::success('Éxito', 'Evento actualizado correctamente')->autoClose(5000)->timerProgressBar();
+            return redirect()->route('calendario.index');
+
+        } catch (\Exception $e) {
+            Alert::error('Error', 'No se pudo actualizar el Evento')->autoClose(5000)->timerProgressBar();
+            return back()->withInput();
+        }
     }
 }
